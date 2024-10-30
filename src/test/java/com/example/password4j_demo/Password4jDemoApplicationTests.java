a7d8778323ec1c5ffe30fa43fdaa339bd86ed5b1
Jorge Reyes <jorgereyes.chavez@gmail.com>
2024-10-29 18:22:20 -0700

    initial commit

---
+++ src/test/java/com/example/password4j_demo/Password4jDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.password4j_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Password4jDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
